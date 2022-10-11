7195876bee80d7c1460fe7c413f1ac608e37dab9
Diego Vivanco <leox_vm@hotmail.com>
2022-10-11 16:33:56 +0200

    feat: add exception test on controller

--- src/test/java/com/dlvivanco/price/api/infrasctructure/PriceControllerTest.java
+++ src/test/java/com/dlvivanco/price/api/infrasctructure/PriceControllerTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -38,8 +37,11 @@ public class PriceControllerTest {
     @Test
     @DisplayName("Test 1: petición a las 10:00 del día 14 del producto 35455 para la brand 1 (ZARA)")
     public void givenADate20200614T100000_whenFindByBrandIdProductIdBetweenDates_thenReturnAPriceWithHigherPriority() throws Exception {
-        this.mockMvc.perform(get("/price?applicationDate=2020-06-14-10:00:00&productId=35455&brandId=1")
-                        .accept(MediaType.APPLICATION_JSON))
+        this.mockMvc.perform(get("/price")
+                        .param("applicationDate","2020-06-14-10:00:00")
+                        .param("productId","35455")
+                        .param("brandId","1")
+                )
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$").exists())
@@ -52,22 +54,27 @@ public class PriceControllerTest {
     @Test
     @DisplayName("Test 2: petición a las 16:00 del día 14 del producto 35455 para la brand 1 (ZARA)")
     public void givenADate20200614T160000_whenFindByBrandIdProductIdBetweenDates_thenReturnAPriceWithHigherPriority() throws Exception {
-        this.mockMvc.perform(get("/price?applicationDate=2020-06-14-16:00:00&productId=35455&brandId=1")
-                        .accept(MediaType.APPLICATION_JSON))
+        this.mockMvc.perform(get("/price")
+                        .param("applicationDate","2020-06-14-16:00:00")
+                        .param("productId","35455")
+                        .param("brandId","1")
+                )
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$").exists())
                 .andExpect(MockMvcResultMatchers.jsonPath(PRODUCT_ID_JSON_PATH).value(PRODUCT_ID))
                 .andExpect(MockMvcResultMatchers.jsonPath(BRAND_ID_JSON_PATH).value(BRAND_ZARA_CODE))
                 .andExpect(MockMvcResultMatchers.jsonPath(PRICE_JSON_PATH).value(25.45));
-
     }
 
     @Test
     @DisplayName("Test 3: petición a las 21:00 del día 14 del producto 35455 para la brand 1 (ZARA)")
     public void givenADate20200614T210000_whenFindByBrandIdProductIdBetweenDates_thenReturnAPriceWithHigherPriority() throws Exception {
-        this.mockMvc.perform(get("/price?applicationDate=2020-06-14-21:00:00&productId=35455&brandId=1")
-                        .accept(MediaType.APPLICATION_JSON))
+        this.mockMvc.perform(get("/price")
+                        .param("applicationDate","2020-06-14-21:00:00")
+                        .param("productId","35455")
+                        .param("brandId","1")
+                )
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$").exists())
@@ -79,8 +86,11 @@ public class PriceControllerTest {
     @Test
     @DisplayName("Test 4: petición a las 10:00 del día 15 del producto 35455 para la brand 1 (ZARA)")
     public void givenADate20200615T100000_whenFindByBrandIdProductIdBetweenDates_thenReturnAPriceWithHigherPriority() throws Exception {
-        this.mockMvc.perform(get("/price?applicationDate=2020-06-15-10:00:00&productId=35455&brandId=1")
-                        .accept(MediaType.APPLICATION_JSON))
+        this.mockMvc.perform(get("/price")
+                        .param("applicationDate","2020-06-15-10:00:00")
+                        .param("productId","35455")
+                        .param("brandId","1")
+                )
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$").exists())
@@ -92,14 +102,33 @@ public class PriceControllerTest {
     @Test
     @DisplayName("Test 5: petición a las 21:00 del día 16 del producto 35455 para la brand 1 (ZARA)")
     public void givenADate20200616T210000_whenFindByBrandIdProductIdBetweenDates_thenReturnAPriceWithHigherPriority() throws Exception {
-        this.mockMvc.perform(get("/price?applicationDate=2020-06-16-21:00:00&productId=35455&brandId=1")
-                        .accept(MediaType.APPLICATION_JSON))
+        this.mockMvc.perform(get("/price")
+                        .param("applicationDate","2020-06-16-21:00:00")
+                        .param("productId","35455")
+                        .param("brandId","1")
+                )
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$").exists())
                 .andExpect(MockMvcResultMatchers.jsonPath(PRODUCT_ID_JSON_PATH).value(PRODUCT_ID))
                 .andExpect(MockMvcResultMatchers.jsonPath(BRAND_ID_JSON_PATH).value(BRAND_ZARA_CODE))
                 .andExpect(MockMvcResultMatchers.jsonPath(PRICE_JSON_PATH).value(38.95));
+    }
+
+    @Test
+    @DisplayName("Test 6: BadRequest cuando no se le pasa un parámetro obligatorio")
+    public void givenABadRequest_whenRequiredParameterIsNotSent() throws Exception {
+        this.mockMvc.perform(get("/price?applicationDate=2020-06-16-21:00:00&productId=35455"))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("Test 7: Not Found cuando no se le pasa un brandId diferente de 1")
+    public void givenANotFound_whenBrandIdNotEqualsTo1() throws Exception {
+        this.mockMvc.perform(get("/price?applicationDate=2020-06-16-21:00:00&productId=35455&brandId=5"))
+                .andDo(print())
+                .andExpect(status().isNotFound());
     }
 
 }
